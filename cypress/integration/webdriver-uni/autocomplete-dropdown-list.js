18d8c7ceb4bd96952756b22ad0028b90b9a9b363
denizbabayaka <denizbabayaka@gmail.com>
2022-04-10 14:26:43 -0400

    @ new files added

---
+++ cypress/integration/webdriver-uni/autocomplete-dropdown-list.js
@@ -0,0 +1,48 @@
+/// <reference types="Cypress" />
+
+
+
+describe("Verify Autocomplete dropdown lists via webdriveruni", () => {
+    it("Select specific values via select dropdown list ", () => {
+        cy.visit("https://webdriveruniversity.com");
+        cy.get('#autocomplete-textfield').invoke('removeAttr', 'target').click({ force: true })
+        cy.get('#myInput').type('A')
+        //This locator will capture all the options in the dropdown list starting with A and
+        //assign it to $el variable
+        cy.get('#myInputautocomplete-list > *').each(($el, index, $list) => {
+            //This will get all the options in the dropdown list as text 
+            const prod = $el.text()
+            const productToSelect = 'Avacado';
+            if (prod === productToSelect) {
+                $el.click();
+                cy.get('#submit-button').click();
+                cy.url().should('include', productToSelect)
+            }
+            // instead of writing a it block we can continue out test with diffrent parameters
+            //by using then actually we can chain the test
+        }).then(() => {
+
+
+            cy.get('#myInput').type('g')
+            // this will iterate through the dropdown list through each method 
+            // and assign it to $el variable
+            cy.get('#myInputautocomplete-list > *').each(($el, index, $list) => {
+                //This will get all the options in the dropdown list as text 
+                const prod = $el.text()
+                const productToSelect = 'Grapes';
+                if (prod === productToSelect) {
+                    $el.click();
+                    cy.get('#submit-button').click();
+                    cy.url().should('include', productToSelect)
+                }
+            })
+
+
+
+
+        })
+    })
+
+
+});
+
---
+++ cypress/integration/webdriver-uni/select-dropdown-list.js
@@ -0,0 +1,25 @@
+/// <reference types="Cypress" />
+
+
+
+describe("Interact with dropdown lists via webdriveruni", () => {
+    it("Select specific values via select dropdown list ", () => {
+        cy.visit("https://webdriveruniversity.com");
+        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({ force: true })
+         //cy.get('#dropdowm-menu-1').select('c#')
+        // cy.get('#dropdowm-menu-2').select('testng').should('have.value', 'testng')
+        // //cy.get('#dropdowm-menu-3').select('javascript')
+        // //select based on tex option in DOM 
+        // cy.get('#dropdowm-menu-3').select('JQuery').contains('JQuery') 
+        
+
+        //asserting the value of dropdown list
+        cy.get('#dropdowm-menu-2').select('maven').should('have.value', 'maven')
+        //asserting the text of dropdown list
+        cy.get('#dropdowm-menu-2').select('TestNG').contains('TestNG')
+        
+
+
+
+    });
+})
