76226b6d84ce97ddf381ad4f5af0651eb70b2e27
denizbabayaka@gmail.com <denizbabayaka@gmail.com>
2022-04-14 15:56:00 -0400

    new files added

--- cypress/integration/automation-test-store/add-multiple-items-to-basket.js
+++ cypress/integration/automation-test-store/add-multiple-items-to-basket.js
@@ -1,9 +1,14 @@
+import AutoStore_Homepage_PO from '../../support/pageObjects/automation-test-store/AutoStore_Homepage_PO';
+
 /// <reference types="cypress" />
 
 
 describe("Add multiple items to basket", () => {
+    const autoStore_Homepage_PO = new AutoStore_Homepage_PO();
+
+
     before(function () {
-        
+
         //This fixture method will assign whatever  inside the array at  product.json file
         // to the  data variable 
         cy.fixture('product').then(function (data) {
@@ -13,20 +18,22 @@ describe("Add multiple items to basket", () => {
 
     })
     beforeEach(function () {
-        cy.visit("https://automationteststore.com/");
-        cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
-      });
+        // cy.visit("https://automationteststore.com/");
+        // cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
+        //Instead of hard coding we will use our custom command
+        autoStore_Homepage_PO.accessHomePage();
+    });
     it("Log information of all hair care products", () => {
         //This will iterate through the array productName1 array inside the product.json file
         //and assign all of them  to the variable element 
-        
-        
+
+
         globalThis.data.productName1.forEach(function (element) {
             // and then we will pass it as an argument to addProductTobasket method coming from
             //cypress commands.js file 
             cy.addProductToBasket(element);
         })
         cy.get('.dropdown-toggle > .fa').click();
-     
+
     });
 });
--- cypress/support/pageObjects/automation-test-store/AutoStore_Homepage_PO.js
+++ cypress/support/pageObjects/automation-test-store/AutoStore_Homepage_PO.js
@@ -7,12 +7,15 @@ class AutoStore_Homepage_PO {
         cy.visit(Cypress.env("automationteststore_homepage"));
         cy.get("a[href*='product/category&path=']").contains("Hdir Care").click();
 
-        
-
-
-    
     }
 
+    clickOn_HairCare_Link() {
+        cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
 
+
+
+    }
+
+}
 
 export default  AutoStore_Homepage_PO;
