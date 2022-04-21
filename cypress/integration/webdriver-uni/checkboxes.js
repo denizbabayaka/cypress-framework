5446314fd31545fef4a4207a5a8e56dc1dba87ee
denizbabayaka@gmail.com <denizbabayaka@gmail.com>
2022-04-21 11:07:51 -0400

    new line of code added

--- cypress/integration/webdriver-uni/checkboxes.js
+++ cypress/integration/webdriver-uni/checkboxes.js
@@ -3,7 +3,15 @@
 
 
 describe("Verify checkboxes via webdriveruni", () => {
-    it("Check and validate checkbox", () => {
+    it("Check and validate checkbox", {
+        //This method will run again if the test fails
+        retries: {
+            runMode: 2,
+            openMode: 1
+
+        }
+
+    }, () => {
         cy.visit("https://webdriveruniversity.com");
         cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({ force: true })
         // it will check the first checkbox and then validate it in line below
@@ -11,7 +19,7 @@ describe("Verify checkboxes via webdriveruni", () => {
         // cy.get('#checkboxes > :nth-child(1) > input').check().should('be.checked') 
         // implementing code has to be order regarding options Line 13 and 16 otherwise 
         //it will not work
-        cy.get('#checkboxes > :nth-child(1) > input').as('option1')
+        cy.get('#checkboxes1 > :nth-child(1) > input').as('option1')
         cy.get('@option1').check()
         cy.get('@option1').check().should('be.checked')
         cy.get(':nth-child(5) > input').as('option3').as('option3')
