3c614d6386b9a93815e15304d6c2ecc2cfe1a2ef
denizbabayaka@gmail.com <denizbabayaka@gmail.com>
2022-04-21 10:08:33 -0400

    new files added

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -12,11 +12,25 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-/**
- * @type {Cypress.PluginConfig}
- */
-// eslint-disable-next-line no-unused-vars
+const fs = require('fs-extra')
+const path = require('path')
+
+function getConfigurationByFile (file) {
+  const pathToConfigFile = path.resolve('cypress', 'config', `${file}.json`)
+  
+  if (!fs.existsSync(pathToConfigFile)) {
+      console.log('No custom config file found')
+      return {};
+  
+ }
+
+   return fs.readJson(pathToConfigFile)
+}
+// plugins file
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+ // accept a configFile value or use development by default
+const file = config.env.configFile 
+
+return getConfigurationByFile(file)
+ 
 }
